2ef34ac5a853e429698ba051858eeb37995fd4cf
tianmarillio <tiansyah.marillio@gmail.com>
2021-04-21 12:28:52 +0700

    relasi antar model belum ada

---
+++ migrations/20210421044329-create-shipment-city.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ShipmentCities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ShipmentId: {
+        type: Sequelize.INTEGER
+      },
+      CityId: {
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ShipmentCities');
+  }
+};
