9eaf7976e57e5b544f4bf924eba95f634e283ecb
tianmarillio <tiansyah.marillio@gmail.com>
2021-04-23 01:47:58 +0700

    revisi

--- app.js
+++ app.js
@@ -76,67 +76,6 @@ app.post("/login", (req, res) => {
     });
 });
 
-// app.use(
-//   session({
-//     secret: "fssoeifjof393823l2k4n12",
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       maxAge: 120000,
-//     },
-//   })
-// );
-
-//   if (req.session.isLoggedIn === true) {
-//     User.findAll({
-//       where: {
-//         username: req.session.username,
-//       },
-//     }).then((data) => {
-//       res.render("halamanUser", {
-//         username: req.session.username,
-//         data,
-//       });
-//     });
-//   } else {
-//     res.redirect("/login");
-//   }
-// });
-// app.get("/login", (req, res) => {
-//   if (req.query.err) {
-//     res.render("login", {
-//       errorLogin: true,
-//     });
-//   } else {
-//     res.render("login", {
-//       errorLogin: false,
-//     });
-//   }
-// });
-// app.post("/login", (req, res) => {
-//   User.findOne({
-//     where: {
-//       username: req.body.username,
-//       password: req.body.password,
-//     },
-//   })
-//     .then((data) => {
-//       if (data === null) {
-//         res.redirect("/login?err=true");
-//       } else {
-//         req.session.isLoggedIn = true;
-//         req.session.username = data.username;
-
-//         res.redirect("/");
-//       }
-//     })
-//     .catch((err) => {
-//       res.send(err);
-//     });
-// });
-
-// app.use(router);
-
 app.listen(port, () => {
   console.log(`Running port : ${port}`);
 });
